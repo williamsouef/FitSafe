5a04a4ccc139f03f16a039b6f3c3805436ca7469
WILLIAM SOUEF <williamsouef@gmail.com>
2023-02-07 07:23:22 +0100

    Initial Commit

---
+++ FitSafe/FitSafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitSafeApp.swift
+//  FitSafe
+//
+//  Created by William Souef on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FitSafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
